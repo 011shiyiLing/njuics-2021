75bebe58c8bdcc309c81212049f495c7c0fb70a7
tracer-ics2022 <tracer@njuics.org>
2022-10-04 23:11:06 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:11:06 up  2:09,  1 user,  load average: 0.39, 0.16, 0.05

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -166,7 +166,7 @@ bool check_parentheses(int p,int q)
 //判断操作符优先级(C语言标准)
 int op_priority(int i)
 {
-  if (tokens[i].type == TK_NEG || tokens[i].type == DEREF) return 1;
+  if (tokens[i].type == TK_NEG) return 1;
   if (tokens[i].type == '!') return 2;
   if (tokens[i].type == '*' || tokens[i].type == '/') return 3;
   if (tokens[i].type == '+' || tokens[i].type == '-') return 4;
