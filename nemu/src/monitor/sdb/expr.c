907eef5b5b81e94ccf4a99c2b4a9c2bffc3fc60c
tracer-ics2022 <tracer@njuics.org>
2022-10-05 09:19:16 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:19:16 up 6 min,  1 user,  load average: 0.91, 0.74, 0.38

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -267,7 +267,7 @@ word_t eval(int p,int q,bool *success)
     int op = find_main_operator(p,q);
     //printf("%d\n",op);
     word_t val1 = 0;
-    word_t val2 = 0;
+    paddr_t val2 = 0;
     if(tokens[op].type != TK_NEG && tokens[op].type != DEREF) val1 = eval(p,op-1,success);
     val2 = eval(op+1,q,success);
     
