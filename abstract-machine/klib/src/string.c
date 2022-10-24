aafb74b6b44a85f42450cbfc72fbc88d8c74caf9
tracer-ics2022 <tracer@njuics.org>
2022-10-24 20:36:31 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:36:31 up  3:10,  1 user,  load average: 0.13, 0.28, 0.29

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -70,16 +70,14 @@ char *strcat(char *dst, const char *src) {
 int strcmp(const char *s1, const char *s2) {
   assert(s1 && s2);
 
-  while(*s1 == *s2)
+  while((*s1 != '\0') && (*s1 == *s2))
   {
-    if(*s1 == '\0') return 0;
     s1++;
     s2++;
   }
 
   if(*s1 < *s2) return -1;
   else if(*s1 > *s2) return 1;
-
   return 0;
 }
 
