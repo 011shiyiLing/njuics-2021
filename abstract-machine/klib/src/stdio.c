dbd7ba853711161f5627bf2059ac2246737a4ad4
tracer-ics2022 <tracer@njuics.org>
2022-10-24 23:15:41 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:15:41 up  5:49,  1 user,  load average: 0.29, 0.17, 0.23

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,62 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
+char *itoa(int value, char *str, int radix)
+{
+  char reverse[36];
+  char *p = reverse;
+  int sign = (value >= 0)? 1:0;
+
+  value = (value >= 0) ? value : -value;
+  *p++ = '\0';
+  while(value >= 0)
+  {
+    *p++ = "0123456789abcdef"[value%radix];
+    value /= radix;
+    if (value == 0) break;
+  }
+
+  if(!sign)
+  {
+    *p++ = '-';
+  }
+  else
+  {
+    p--;
+  } 
+
+  while (p >= reverse)
+  {
+    *str++ = *p--;
+  }
+  return str;
+}
+
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  char *s;
+  int d;
+
+  va_start(args,fmt);
+  while(*fmt)
+  {
+    switch (*fmt++)
+    {
+      case 's':
+        s = va_arg(args,char *);
+        strcat(out,s);
+        break;
+      case 'd':
+        d = va_arg(args,int);
+        out = itoa(d,out,10);
+        break;
+      default:
+        return -1;
+    }
+  }
+  
+ va_end(args);
+ return -1;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
