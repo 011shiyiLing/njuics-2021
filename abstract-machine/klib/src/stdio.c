81cc3470aa37c7d15b63563be493e8e997d48e07
tracer-ics2022 <tracer@njuics.org>
2022-10-25 16:49:30 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:49:30 up  2:26,  1 user,  load average: 0.25, 0.22, 0.25

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -74,7 +74,7 @@ int sprintf(char *out, const char *fmt, ...) {
         break;
       case 'd':
         d = va_arg(args,int);
-        p = itoa(d,p,10);
+        itoa(d,p,10);
         p += strlen(p);
         break;
       default:
