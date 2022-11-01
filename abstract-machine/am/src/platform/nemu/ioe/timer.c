366b0f08f15caf32e668a73b75e0db40eb293528
tracer-ics2022 <tracer@njuics.org>
2022-11-01 22:32:58 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:32:58 up  2:29,  1 user,  load average: 0.39, 0.29, 0.22

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -7,7 +7,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   //__am_timer_init();
-  uint32_t now_time = inl(RTC_ADDR);
+  int now_time = inl(RTC_ADDR);
   uptime->us = (uint64_t)(now_time);
 }
 
