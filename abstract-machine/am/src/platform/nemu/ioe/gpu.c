0bc74fb208f55f37f777a496e4647aa367df59b7
tracer-ics2022 <tracer@njuics.org>
2022-11-06 12:20:56 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:20:56 up  1:24,  1 user,  load average: 0.07, 0.17, 0.14

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+#include <string.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 #define N 32
@@ -23,6 +24,15 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (w == 0 || h == 0) return;
+
+  int cp_bytes = w * sizeof(uint32_t);
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for(int j=0;j<h;j++)
+  {
+    memcpy(&fb[(y+j)*w+x],ctl->pixels,cp_bytes);
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR,1);
   }
