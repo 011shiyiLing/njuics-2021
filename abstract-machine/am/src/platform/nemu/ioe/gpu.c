804ed0be91ca4461cb1d71c42c5e74d29fef1aa1
tracer-ics2022 <tracer@njuics.org>
2022-11-09 11:34:21 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:34:21 up  1:46,  1 user,  load average: 1.10, 1.22, 1.18

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -32,7 +32,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for(int j=0;j<h && y+j < H;j++)
   {
-    memcpy(&fb[x*y],pixels,cp_bytes);
+    memcpy(&fb[(y+j)*W+x],pixels,cp_bytes);
     pixels += w;
   }
 
