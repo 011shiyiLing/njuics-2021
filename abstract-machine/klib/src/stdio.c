afdcc3c0333d2761f1d3af293db5355555203b31
tracer-ics2022 <tracer@njuics.org>
2022-11-10 19:27:40 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:27:40 up  1:13,  1 user,  load average: 0.28, 0.23, 0.21

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -76,12 +76,12 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   
     switch (*fmt)
     {
-      /*case 's':
+      case 's':
         *p = '\0';
         strcat(p,va_arg(ap,char *));
         p += strlen(p);
         break;
-      case 'd':
+      /*case 'd':
         itoa(va_arg(ap,int),p,10);
         p += strlen(p);
         break;
