4ba0132636f9643a43940e2207a27563c3e48bda
tracer-ics2022 <tracer@njuics.org>
2022-11-10 20:39:06 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:39:06 up  2:25,  1 user,  load average: 0.36, 0.30, 0.33

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -72,6 +72,19 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
     }
 
     fmt++;
+
+    if(*fmt == '0')
+    {
+      fmt ++;
+      int bit = atoi(fmt);
+      for(int i=0;i<bit; i++)
+      {
+        *p = '0';
+        p++;
+      }
+      fmt++;
+      continue;
+    }
   
     switch (*fmt)
     {
@@ -88,8 +101,6 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
         uitoa(va_arg(ap,uint32_t),p,16);
         p += strlen(p);
         break;
-      case '0':
-        *p++ = '0';
       default:
         break;
     }
