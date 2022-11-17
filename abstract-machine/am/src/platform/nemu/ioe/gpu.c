3f8cba119165a4636946b9bf15e781619e6419a9
tracer-ics2022 <tracer@njuics.org>
2022-11-17 23:00:43 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:00:42 up  4:54,  1 user,  load average: 0.10, 0.26, 0.26

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -16,6 +16,7 @@ void __am_gpu_init() {
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   uint32_t info = inl(VGACTL_ADDR);
+  //从VVGACTL_ADDR这个内存映射IO地址读到的数据高16位是宽度信息，低16位是高度信息
   uint16_t width = (uint16_t)(info >> 16);
   uint16_t height = (uint16_t)(info & 0xffff);
 
@@ -33,15 +34,29 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
 
   int win_weight = io_read(AM_GPU_CONFIG).width;
+  int win_height = io_read(AM_GPU_CONFIG).height;
 
-  //int cp_bytes = w * sizeof(uint32_t);
-  
-  for(int i=0; i<h; ++i)
+  /*for(int i=0; i<h; ++i)
   {
     for(int j=0; j<w; ++j)
     {
       fb[y*win_weight + i*win_weight + x + j] = pixels[i*w + j];
     }
+  }*/
+  
+  int cp_bytes = 0;
+  if(w < win_weight - x)
+  {
+    cp_bytes = sizeof(uint32_t)*w;
+  }
+  else{
+    cp_bytes = sizeof(uint32_t)*(win_weight-x);
+  }
+
+  for(int j=0; j<h && (y+j) < win_height; j++)
+  {
+    memcpy(&fb[(y+j)*win_height + x],pixels,cp_bytes);
+    pixels += w;
   }
 
   if (ctl->sync) {
