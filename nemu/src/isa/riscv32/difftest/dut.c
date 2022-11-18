9fd9f72efb7a5bfe412217c4aa14bb1392a34b04
tracer-ics2022 <tracer@njuics.org>
2022-11-18 15:30:00 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:30:00 up  1:27,  1 user,  load average: 0.38, 0.24, 0.15

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,12 +18,20 @@
 #include <memory/vaddr.h>
 #include "../local-include/reg.h"
 
+const char *regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  CPU_state r = *ref_r;
-  if(r.pc != pc) return false;
+  bool res = difftest_check_reg("pc", cpu.pc, ref_r->pc, cpu.pc);
+
   for(int i=0;i<32;i++)
   {
-    if(r.gpr[i] != cpu.gpr[i]) return false;
+    res = difftest_check_reg(regs[i], cpu.pc, ref_r->gpr[i], cpu.gpr[i]);
+    if(!res) return res;
   }
   return true;
 }
