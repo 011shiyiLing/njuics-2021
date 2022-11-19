a69a1c002af86d22571ac3d083623e9327101ca1
tracer-ics2022 <tracer@njuics.org>
2022-11-19 18:01:30 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:01:30 up  7:43,  1 user,  load average: 0.49, 0.44, 0.35

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -101,8 +101,8 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  //assert(s);
-  //if(n < 0) assert(0);
+  assert(s);
+  if(n < 0) assert(0);
 
   char* temp = (char*) s;
   while (n != 0)
@@ -112,7 +112,7 @@ void *memset(void *s, int c, size_t n) {
     n --;
   }
   *temp = '\0';
-  return s;
+  return temp;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
