ee795de9a110aa7a3d49c29130cf3609d6209d5f
tracer-ics2022 <tracer@njuics.org>
2022-11-19 17:56:26 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:56:26 up  7:38,  1 user,  load average: 0.45, 0.36, 0.29

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -23,6 +23,12 @@
 static uint8_t *io_space = NULL;
 static uint8_t *p_space = NULL;
 
+//dtrace
+void dtrace()
+{
+
+}
+
 uint8_t* new_space(int size) {
   uint8_t *p = p_space;
   // page aligned;
@@ -58,6 +64,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+  IFDEF(CONFIG_DTRACE,printf("%s : %d\n",map->name,addr));
   return ret;
 }
 
@@ -67,4 +74,5 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
+  IFDEF(CONFIG_DTRACE,printf("%s : %d\n",map->name,addr));
 }
