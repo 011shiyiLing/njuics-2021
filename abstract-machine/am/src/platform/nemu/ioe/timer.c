ee957e4de02a2e056e7b51fc819d1d6275f6063b
tracer-ics2022 <tracer@njuics.org>
2022-11-20 19:07:04 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:07:04 up 46 min,  1 user,  load average: 0.45, 0.49, 0.49

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,18 +1,18 @@
 #include <am.h>
 #include <nemu.h>
 
-//static uint64_t boot_time;
+static uint64_t boot_time;
 void __am_timer_init() {
-  //boot_time = (uint64_t)(inl(RTC_ADDR));
-  //printf("%d\n",boot_time);
+  boot_time = (uint64_t)(inl(RTC_ADDR));
+  printf("%d\n",boot_time);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  //__am_timer_init();
-  //uint64_t now_time = (uint64_t)(inl(RTC_ADDR + 4));
+  __am_timer_init();
+  uint64_t now_time = (uint64_t)(inl(RTC_ADDR + 4));
   //printf("%d\n",now_time);
-  //uptime->us = boot_time - now_time;
-  uptime->us = ((uint64_t)inl(RTC_ADDR+4)<<32) | (uint64_t)inl(RTC_ADDR);
+  uptime->us = boot_time - now_time;
+  //uptime->us = ((uint64_t)inl(RTC_ADDR+4)<<32) | (uint64_t)inl(RTC_ADDR);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
