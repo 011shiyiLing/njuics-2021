2c24bfda284c0a793189f44c6c31a3a29d26a013
tracer-ics2022 <tracer@njuics.org>
2022-11-26 15:04:35 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:04:35 up  5:58,  1 user,  load average: 0.36, 0.32, 0.31

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    //case EVENT_YIELD:printf("Yield Event!\n");break;
+    case EVENT_YIELD:printf("Yield Event!\n");break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
