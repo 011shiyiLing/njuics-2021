d55fe5cae3457a7a0684d0686f93c1e644ffcc84
tracer-ics2022 <tracer@njuics.org>
2022-11-26 11:38:20 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:38:20 up  2:32,  1 user,  load average: 0.14, 0.28, 0.25

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -112,7 +112,7 @@ int sprintf(char *out, const char *fmt, ...) {
 
 int printf(const char *fmt,...)
 {
-  char buf[256];
+  char buf[1024];
   va_list args;
   memset(buf,0,sizeof(buf));
   va_start(args,fmt);
