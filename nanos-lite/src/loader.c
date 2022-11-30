5c64c3cccc4ca772b5bc6d25ff872d285cb5ee0b
tracer-ics2022 <tracer@njuics.org>
2022-11-30 18:53:54 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:53:54 up  4:29,  1 user,  load average: 0.63, 0.30, 0.23

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -21,8 +21,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   for(int i=0; i<ehdr.e_phnum;i++)
   {
     if(phdr[i].p_type != PT_LOAD) continue;
-    ramdisk_read((void *)(phdr[i].p_vaddr),phdr[i].p_offset,phdr[i].p_memsz);//
-    memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz),0,(phdr[i].p_memsz-phdr[i].p_filesz));//将[VirtAddr + FileSiz, VirtAddr + MemSiz)对应的物理区间清零.
+    ramdisk_read((void *)(phdr[i].p_paddr),phdr[i].p_offset,phdr[i].p_memsz);//
+    memset((void *)(phdr[i].p_paddr + phdr[i].p_filesz),0,(phdr[i].p_memsz-phdr[i].p_filesz));//将[VirtAddr + FileSiz, VirtAddr + MemSiz)对应的物理区间清零.
   }
 
   //return 0;
