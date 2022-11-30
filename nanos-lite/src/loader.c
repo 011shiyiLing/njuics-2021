3fac859281b27cba7076a3464c0f14f36656df66
tracer-ics2022 <tracer@njuics.org>
2022-11-30 18:43:44 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:43:44 up  4:19,  1 user,  load average: 0.01, 0.16, 0.23

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,10 +8,25 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  //TODO();
+  Elf_Ehdr ehdr;
+  Elf_Phdr phdr; //programming header table
+  ramdisk_read(&ehdr,0,sizeof(ehdr));
+  ramdisk_read(&phdr,ehdr.e_ehsize,sizeof(phdr)*ehdr.e_phnum);
+  assert(*(uint32_t *)ehdr.e_ident == 0x7f454c46); // check magic number
+
+  for(int i=0; i<ehdr.e_phnum;i++)
+  {
+    if(phdr.p_type != PT_LOAD) continue;
+    ramdisk_read((void *)(phdr.p_vaddr),phdr.p_offset,phdr.p_memsz);//
+    memset((void *)(phdr.p_vaddr + phdr.p_filesz),0,phdr.p_memsz-phdr.p_filesz);//将[VirtAddr + FileSiz, VirtAddr + MemSiz)对应的物理区间清零.
+  }
+
+  //return 0;
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
