48cea4e8ea362fc13eded2b0f437eb3a044c7b7d
tracer-ics2022 <tracer@njuics.org>
2022-12-02 14:22:05 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:22:05 up 18 min,  1 user,  load average: 0.24, 0.41, 0.36

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,11 +8,26 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
+
+//check ISA type
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_X86__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined (__ISA_MIPS32__)
+# define EXPECT_TYPE EM_MIPS
+#elif defined(__ISA_RISCV32__) || defined(__ISA_RISCV64__)
+# define EXPECT_TYPE EM_RISCV
+#else
+# error Unsupported ISA
+#endif
+
+// 从ramdisk中`offset`偏移处的`len`字节读入到`buf`中
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
-  Elf_Ehdr ehdr;
+  Elf_Ehdr ehdr; //FLF header
   ramdisk_read(&ehdr,0,sizeof(Elf_Ehdr));
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f); //检查魔数（注意是小端排列）
 
