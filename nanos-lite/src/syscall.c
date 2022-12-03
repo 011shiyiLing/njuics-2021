2150005316e74b66ed26417a1e2c7677a4dd6ace
tracer-ics2022 <tracer@njuics.org>
2022-12-03 13:12:42 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:12:41 up  2:59,  1 user,  load average: 0.50, 0.48, 0.40

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -21,10 +21,7 @@ int sys_write(int fd,void * buf,size_t count)
     }
     return count;
   }
-  else
-  {
-    return -1;
-  }
+  return -1;
 }
 
 //STRACE(system call trace)
@@ -54,54 +51,6 @@ void do_syscall(Context *c) {
     case 4://SYS_write
       c->GPRx = sys_write((int)a[1],(void *)a[2],(size_t)a[3]);
       break;
-    /*case 2: //SYS_open
-      c->GPRx = _open((const char *)c->GPR2,(int)c->GPR3,(mode_t)c->GPR4);
-      break;
-    case 3://SYS_read
-      c->GPRx = _read((int)c->GPR2,(void *)c->GPR3,(size_t)c->GPR4);
-      break;
-    case 5://SYS_kill
-      c->GPRx = _kill((int)c->GPR2,(int)c->GPR3);
-      break;
-    case 6://SYS_getpid
-      c->GPRx = _getpid();
-      break;
-    case 7://SYS_close
-      c->GPRx = _close((int)c->GPR2);
-      break;
-    case 8://SYS_lseek
-      c->GPRx = _lseek((int)c->GPR2,(off_t)c->GPR3,(int)c->GPR4);
-      break;
-    case 9://SYS_brk
-      c->GPRx = _sbrk((intptr_t)c->GPR2);
-      break;
-    case 10://SYS_fstat
-      c->GPRx = _fstat((int)c->GPR2,(struct stat*)c->GPR3);
-      break;
-    case 11://SYS_time
-      break;
-    case 12://SYS_signal
-      break;
-    case 13://SYS_execve
-      c->GPRx = _execve((const char*)c->GPR2,(char * const*)c->GPR3,(char * const*)c->GPR4);
-      break;
-    case 14://SYS_fork
-      c->GPRx = _fork();
-      break;
-    case 15://SYS_link
-      c->GPRx = _link((const char *)c->GPR2,(const char*)c->GPR3);
-      break;
-    case 16://SYS_unlink
-      c->GPRx = _unlink((const char*)c->GPR2);
-      break;
-    case 17://SYS_wait
-      c->GPRx = _wait((int *)c->GPR2);
-    case 18://SYS_times
-      c->GPRx = _times((void *)c->GPR2);
-      break;
-    case 19://SYS_gettimeofday
-      c->GPRx = _gettimeofday((struct timeval *)c->GPR2,(struct timezone *)c->GPR3);
-      break;*/
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
