b53a481fc8a0ca7a0c24f23cb38fae4826be7d72
tracer-ics2022 <tracer@njuics.org>
2022-12-04 20:45:32 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:45:32 up  3:20,  1 user,  load average: 0.12, 0.21, 0.24

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -23,6 +23,11 @@ int sys_write(int fd,void *buf,size_t count)
   return -1;
 }
 
+int sys_brk(void *addr)
+{
+  return 0;
+}
+
 
 //STRACE(system call trace)
 static void strace(Context *c)
@@ -52,7 +57,7 @@ void do_syscall(Context *c) {
       c->GPRx = sys_write((int)a[1],(void *)a[2],(size_t)a[3]);
       break;
     case 9://SYS_brk
-      c->GPRx = 0;
+      c->GPRx = (int)sys_brk((void *)a[1]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
