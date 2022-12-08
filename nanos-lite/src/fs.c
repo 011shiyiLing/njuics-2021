cd07fd4af7ee4620854a58efc6b73aaa744f3cbc
tracer-ics2022 <tracer@njuics.org>
2022-12-08 21:11:12 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:11:12 up  3:29,  1 user,  load average: 0.59, 0.54, 0.46

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -130,6 +130,6 @@ size_t fs_lseek(int fd,size_t offset,int whence)
 int fs_close(int fd)
 {
   file_table[fd].open_offset = 0;
-  Log("File name close:%s\n",file_table[fd].name);
+  Log("File close:%s\n",file_table[fd].name);
   return 0;
 }
