1adfa515b8e163f0d5f43797ceb1eadd7247c2d3
tracer-ics2022 <tracer@njuics.org>
2022-12-08 19:19:28 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:19:28 up  1:37,  1 user,  load average: 0.48, 0.47, 0.46

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -30,9 +30,9 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write,0},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write,0},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write,0},
 #include "files.h"
 };
 
@@ -61,15 +61,15 @@ size_t fs_write(int fd,const void *buf,size_t len)
   size_t count;
   if(fd == 1 || fd == 2)
   {
-    return file_table[fd].write(buf,file_table[fd].open_offset,len);
-    //file_table[fd].open_offset += count;
+    count = file_table[fd].write(buf,file_table[fd].open_offset,len);
+    file_table[fd].open_offset += count;
   }
   else
   {
     if(file_table[fd].size - file_table[fd].open_offset <= len) assert(0);
     count = len;
-    ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].open_offset,count);
-    file_table[fd].open_offset += count;
+    ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
+    file_table[fd].open_offset += len;
   }
   return count;
 }
@@ -77,10 +77,10 @@ size_t fs_write(int fd,const void *buf,size_t len)
 size_t fs_read(int fd,void *buf,size_t len)
 { 
   size_t count;
-  if(file_table[fd].read)
+  if(fd == 0 || fd == 2)
   {
-    return file_table[fd].read(buf,file_table[fd].open_offset,len);
-    //file_table[fd].open_offset += count;
+    count = file_table[fd].read(buf,file_table[fd].open_offset,len);
+    file_table[fd].open_offset += count;
   }
   else
   {
