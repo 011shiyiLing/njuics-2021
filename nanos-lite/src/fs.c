7365e4c3a4b081a910c09e0803bf23e9452901b3
tracer-ics2022 <tracer@njuics.org>
2022-12-08 17:47:17 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:47:17 up 5 min,  1 user,  load average: 0.36, 0.44, 0.23

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -3,18 +3,23 @@
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
+  ////为每一个已经打开的文件引入偏移量属性open_offset, 来记录目前文件操作的位置. 每次对文件读写了多少个字节, 偏移量就前进多少
+  size_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
-  panic("should not reach here");
+  panic("should not reach here"); //panic:系统发现无法继续运行下去的故障时将调用它，会导致程序中止，然后由系统显示错误号
   return 0;
 }
 
@@ -33,4 +38,89 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+//通过open()系统调用来打开一个文件, 并返回相应的文件描述符
+int fs_open(const char *pathname,int flags,int mode)
+{
+  int n = (sizeof(file_table)/sizeof(file_table[0]));
+  int i;
+  for(i=0;i<n;i++)
+  {
+    if(strcmp(file_table[i].name,pathname) == 0)
+    {
+      file_table[i].open_offset = 0;
+      return i;
+    }
+  }
+  assert(0);
+}
+
+size_t fs_wirte(int fd,const void *buf,size_t len)
+{
+  size_t count;
+  if(file_table[fd].write != NULL)
+  {
+    count = file_table[fd].write(buf,file_table[fd].open_offset,len);
+    file_table[fd].open_offset += count;
+  }
+  else
+  {
+    if(file_table[fd].size - file_table[fd].open_offset <= len) assert(0);
+    count = len;
+    ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].open_offset,count);
+    file_table[fd].open_offset += count;
+  }
+  return count;
+}
+
+size_t fs_read(int fd,void *buf,size_t len)
+{ 
+  size_t count;
+  if(file_table[fd].read != NULL)
+  {
+    count = file_table[fd].read(buf,file_table[fd].open_offset,len);
+    file_table[fd].open_offset += count;
+  }
+  else
+  {
+    if(file_table[fd].open_offset + len <= file_table[fd].size)
+    {
+      count = len;
+    }
+    else
+    {
+      count = file_table[fd].size - file_table[fd].open_offset;
+    }
+
+    ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,count);
+    file_table[fd].open_offset += count;
+  }
+  return count;
+}
+
+//调整偏移量
+size_t fs_lseek(int fd,size_t offset,int whence)
+{
+  switch (whence)
+  {
+  case SEEK_SET:
+    file_table[fd].open_offset = offset;
+    break;
+  case SEEK_CUR:
+    file_table[fd].open_offset += offset;
+    break;
+  case SEEK_END:
+    file_table[fd].open_offset = file_table[fd].size + offset;
+    break;
+  default:
+    break;
+  }
+  return file_table[fd].open_offset;
+}
+
+int fs_close(int fd)
+{
+  file_table[fd].open_offset = 0;
+  return 0;
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -24,24 +25,47 @@
 
 // 从ramdisk中`offset`偏移处的`len`字节读入到`buf`中
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
+int fs_open(const char *pathname,int flags,int mode);
+size_t fs_read(int fd,void *buf,size_t len);
+size_t fs_lseek(int fd,size_t offset,int whence);
+int fs_close(int fd);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
   Elf_Ehdr ehdr; //FLF header
-  ramdisk_read(&ehdr,0,sizeof(Elf_Ehdr));
+  //ramdisk_read(&ehdr,0,sizeof(Elf_Ehdr));
+  int fd = fs_open(filename,0,0);
+  size_t ph_offset;
+  ph_offset = fs_read(fd,&ehdr,sizeof(Elf_Ehdr));
+
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f); //检查魔数（注意是小端排列）
 
-  Elf_Phdr phdr[ehdr.e_phnum]; //programming header table
+  /*Elf_Phdr phdr[ehdr.e_phnum]; //programming header table
   ramdisk_read(phdr,ehdr.e_ehsize,sizeof(Elf_Phdr)*ehdr.e_phnum);
 
   for(int i=0; i<ehdr.e_phnum;i++)
   {
     if(phdr[i].p_type != PT_LOAD) continue;
-    ramdisk_read((void *)(phdr[i].p_vaddr),phdr[i].p_offset,phdr[i].p_memsz);//
+    ramdisk_read((void *)(phdr[i].p_vaddr),phdr[i].p_offset,phdr[i].p_memsz);
     memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, (phdr[i].p_memsz-phdr[i].p_filesz));//将[VirtAddr + FileSiz, VirtAddr + MemSiz)对应的物理区间清零.
+  }*/
+
+  Elf_Phdr phdr;
+  for(int i=0;i<ehdr.e_phnum;i++)
+  {
+    ph_offset += fs_read(fd,&phdr,ehdr.e_phentsize);
+    if(phdr.p_type == PT_LOAD)
+    {
+      fs_lseek(fd,phdr.p_offset,SEEK_SET);
+      fs_read(fd,(void *)phdr.p_vaddr,phdr.p_filesz);
+      memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, (phdr.p_memsz-phdr.p_filesz));
+      fs_lseek(fd,ph_offset,SEEK_SET);
+    }
+    else break;
   }
 
   //return 0;
+  fs_close(fd);
   return ehdr.e_entry;
 }
 
