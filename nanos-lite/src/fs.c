4ab6964723e91ebd3a1cceec5edcc1b4868ed149
tracer-ics2022 <tracer@njuics.org>
2022-12-08 19:11:36 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:11:36 up  1:29,  1 user,  load average: 0.52, 0.48, 0.45

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -79,8 +79,8 @@ size_t fs_read(int fd,void *buf,size_t len)
   size_t count;
   if(fd >= 0 && fd <= 2)
   {
-    count = file_table[fd].read(buf,file_table[fd].open_offset,len);
-    file_table[fd].open_offset += count;
+    return file_table[fd].read(buf,file_table[fd].open_offset,len);
+    //file_table[fd].open_offset += count;
   }
   else
   {
