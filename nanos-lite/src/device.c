5aa15e6f5e0f1bfc53427841c70d6ee156e49673
tracer-ics2022 <tracer@njuics.org>
2022-12-16 14:36:16 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:36:16 up 42 min,  1 user,  load average: 0.44, 0.39, 0.36

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -29,7 +29,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   //strcpy(buf,"0");
   static AM_INPUT_KEYBRD_T ev;
   ioe_read(AM_INPUT_KEYBRD,&ev);
-  printf("%d\n",ev.keycode);
+  printf("%d\n",ev.keydown);
   if(ev.keycode == AM_KEY_NONE)
   {
     return 0;
