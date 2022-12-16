c35f03325a52c7d792c6564ff715f3739439af7c
tracer-ics2022 <tracer@njuics.org>
2022-12-16 14:44:38 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:44:38 up 50 min,  1 user,  load average: 0.24, 0.30, 0.33

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -27,8 +27,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 //把事件写入到buf中, 最长写入len字节, 然后返回写入的实际长度.
 size_t events_read(void *buf, size_t offset, size_t len) {
   //strcpy(buf,"0");
-  static AM_INPUT_KEYBRD_T ev;
-  ioe_read(AM_INPUT_KEYBRD,&ev);
+  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   //printf("%d\n",ev.keydown);
   if(ev.keycode == AM_KEY_NONE)
   {
