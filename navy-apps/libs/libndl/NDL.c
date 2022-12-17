740ce95e14f114bb426fdd1cc896d6fc6e8ee941
tracer-ics2022 <tracer@njuics.org>
2022-12-17 15:31:04 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:31:04 up  2:43,  1 user,  load average: 0.21, 0.27, 0.27

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -61,11 +61,9 @@ void NDL_OpenCanvas(int *w, int *h) {
 // 向画布`(x, y)`坐标处绘制`w*h`的矩形图像, 并将该绘制区域同步到屏幕上
 // 图像像素按行优先方式存储在`pixels`中, 每个像素用32位整数以`00RRGGBB`的方式描述颜色
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
-  //int fp = open("/dev/fb",NULL);
-
   for(int i=0; i<h; i++)
   {
-      lseek(fbdev, (y + i)*screen_w + x, SEEK_SET);
+      lseek(fbdev, (y + i)*canvas_w + x, SEEK_SET);
       write(fbdev, pixels + i*w, w);
   }
   write(fbdev , 0, 0);
@@ -89,7 +87,7 @@ int NDL_Init(uint32_t flags) {
   if (getenv("NWM_APP")) {
     evtdev = 3;
   }
-
+  fbdev = open("/dev/fp",0,0);
   //get screen_w and screen_height
   char buf[64];
   int fp = open("/proc/dispinfo",NULL);
