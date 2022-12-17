268d2c37975dc8041baf65169dfad264c932bdbb
tracer-ics2022 <tracer@njuics.org>
2022-12-17 13:46:08 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:46:08 up 58 min,  1 user,  load average: 0.24, 0.22, 0.18

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -14,6 +14,8 @@ static const char *keyname[256] __attribute__((used)) = {
   AM_KEYS(NAME)
 };
 
+static int width,height;
+
 //串口
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   assert(len >= 0);
@@ -53,12 +55,33 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 //获取屏幕大小
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T cfg = io_read(AM_GPU_CONFIG);
-  sprintf(buf,"WIDTH : %d\nHEIGHT : %d\n",cfg.width,cfg.height);
+  width = cfg.width;
+  height = cfg.height;
+  sprintf(buf,"WIDTH : %d\nHEIGHT : %d\n",width,height);
   return strlen(buf);
 }
-
+//用于把buf中的len字节写到屏幕上offset处.
+//需要先从offset计算出屏幕上的坐标, 然后调用IOE来进行绘图
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  AM_GPU_FBDRAW_T fbdraw;
+  if(len == 0)
+  {
+    fbdraw.sync = 1;
+    fbdraw.w = 0;
+    fbdraw.h = 0;
+    ioe_write(AM_GPU_FBDRAW,&fbdraw);
+    return 0;
+  }
+
+  fbdraw.pixels = (void *)buf;
+  fbdraw.w = len;
+  fbdraw.h = 1;
+  fbdraw.x = offset % width;
+  fbdraw.y = offset / width;
+  fbdraw.sync = 0;
+  ioe_write(AM_GPU_FBDRAW,&fbdraw);
+
+  return len;
 }
 
 void init_device() {
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -27,6 +27,7 @@ int NDL_PollEvent(char *buf, int len) {
   int fp = open("/dev/events",NULL);
   return read(fp,buf,len);
 }
+
 // 打开一张(*w) X (*h)的画布
 // 如果*w和*h均为0, 则将系统全屏幕作为画布, 并将*w和*h分别设为系统屏幕的大小
 void NDL_OpenCanvas(int *w, int *h) {
@@ -54,7 +55,19 @@ void NDL_OpenCanvas(int *w, int *h) {
   }
 }
 
+// 向画布`(x, y)`坐标处绘制`w*h`的矩形图像, 并将该绘制区域同步到屏幕上
+// 图像像素按行优先方式存储在`pixels`中, 每个像素用32位整数以`00RRGGBB`的方式描述颜色
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
+  int fp = open("/dev/fb",NULL);
+  for(int i=0; i<h; i++)
+  {
+    for(int j=0; j<w ;j++)
+    {
+      lseek(fp, y*screen_w + i*screen_w + x + j, SEEK_SET);
+      write(fp, pixels[i*w + j], w);
+    }
+  }
+  close(fp);
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
