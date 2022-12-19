c21fb1f1a9ace490f6a40ea4bb8632777f35d47d
tracer-ics2022 <tracer@njuics.org>
2022-12-19 17:51:02 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:51:02 up  5:03,  1 user,  load average: 0.28, 0.43, 0.54

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -5,26 +5,29 @@
 
 int main()
 {
-    //struct timeval timeval;
-    //struct timezone timezone;
-    NDL_Init(0);
+    struct timeval timeval;
+    struct timezone timezone;
+    //NDL_Init(0);
     uint32_t time;
     uint32_t msec = 500;
     //gettimeofday(&timeval,&timezone);
     while(1)
     {
-        time = NDL_GetTicks();
+        //time = NDL_GetTicks();
+        gettimeofday(&timeval,&timezone);
+        time = timeval.tv_sec*1000 - timeval.tv_usec/1000;
         //printf("%d\n",time);
         while(time < msec)
         {
-                //gettimeofday(&timeval,&timezone);
-                time = NDL_GetTicks();
-                //printf("%d\n",time);
+            gettimeofday(&timeval,&timezone);
+            time = timeval.tv_sec*1000 - timeval.tv_usec/1000;
+            //time = NDL_GetTicks();
+            //printf("%d\n",time);
         }
         //gettimeofday(&timeval,&timezone);
         printf("0.5s has passed!\n");
         msec += 500;
     }
-    NDL_Quit();
-    //return 0;
+    //NDL_Quit();
+    return 0;
 }
