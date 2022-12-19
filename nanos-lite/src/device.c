08ccc031ebb81c1ab1f8e4517509f1ffea0710ef
tracer-ics2022 <tracer@njuics.org>
2022-12-19 14:11:28 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:11:28 up  1:24,  1 user,  load average: 0.98, 0.83, 0.68

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -79,7 +79,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 
   fbdraw.pixels = (void *)buf;
   fbdraw.w = len;
-  fbdraw.h = 400;
+  fbdraw.h = height;
   fbdraw.x = (offset) % (width);
   fbdraw.y = (offset) / (width);
   fbdraw.sync = 1;
