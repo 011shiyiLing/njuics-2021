88bb3d18f97e1dad43d60065a96595a8a3bc845c
tracer-ics2022 <tracer@njuics.org>
2022-12-30 19:39:16 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:39:16 up  5:12,  1 user,  load average: 0.25, 0.12, 0.07

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -88,8 +88,7 @@ void naive_uload(PCB *pcb, const char *filename) {
 void context_kload(PCB *pcb, void (*entry)(void *),void *arg)
 { 
   Area kstack;
-  kstack.start = (void *)(&pcb[0]);
-  kstack.end = (void *)(kstack.start + STACK_SIZE);
+  kstack = RANGE(pcb, (char *)pcb + STACK_SIZE);
 
   Context *c = kcontext(kstack,entry,arg);
   pcb->cp = c;
