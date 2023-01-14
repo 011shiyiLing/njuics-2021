74fee87a3938dd71786aaeacfabd9a58dcc8353c
tracer-ics2022 <tracer@njuics.org>
2023-01-14 21:48:44 +0800

    >  compile NEMU 211300075 凌诗怡 Linux shiyiling-NBLK-WAX9X 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:48:44 up 3 days,  6:48,  1 user,  load average: 0.39, 0.58, 0.36

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void hello_fun(void *arg) {
 void init_proc() {
   context_kload(&pcb[0],hello_fun,NULL);
   //context_kload(&pcb[1],hello_fun,"two");
-  //context_uload(&pcb[1],"/bin/pal", NULL, NULL);
+  context_uload(&pcb[1],"/bin/pal", NULL, NULL);
   //context_uload(&pcb[0],"/bin/exec-test",NULL,NULL);
   switch_boot_pcb();
 
